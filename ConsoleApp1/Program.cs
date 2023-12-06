0ecfce56a854c1eb4083819b8bdde3e32e5cbbd9
Pavel <Pavel@DESKTOP-S6TKE73>
2023-12-06 15:12:42 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Numerics;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        ClassWithFunc f = new ClassWithFunc();
+
+        //f.Sort();
+
+        Console.WriteLine("Введите слово: ");
+        string input = Console.ReadLine();
+        Console.WriteLine(f.Polindrom(input));
+
+        Console.WriteLine("Введите число");
+        int inputNum = int.Parse((Console.ReadLine()));
+        Console.WriteLine("Факториал данного числа равен: " + f.Fact(inputNum));
+
+        Console.WriteLine("Введите позицию в строке (от 0 до 4)");
+        int index = int.Parse(Console.ReadLine());
+        if (f.Fib(index) == 0)
+        {
+            Console.WriteLine("Ошибка");
+        }
+        else
+        {
+            Console.WriteLine(f.Fib(index));
+        }
+
+        Console.WriteLine("Введите основание");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите степень");
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine(f.Vesh(a, b));
+
+        Console.WriteLine("Введите число");
+        int i = int.Parse(Console.ReadLine());
+        Console.WriteLine(f.Check(i));
+    }
+}
+
+public class ClassWithFunc
+{
+    int[] mas = new int[] { 3, 5, 2, 7, 1 };
+
+    public static void Sort(ref int[] mas)
+    {
+        int swap = 0;
+        for (int i = 0; i < mas.Length - 1; i++)
+        {
+            for (int j = 0; j < mas.Length - 1 - i; j++)
+            {
+                if (mas[j] > mas[j + 1])
+                {
+                    swap = mas[j + 1];
+                    mas[j + 1] = mas[j];
+                    mas[j] = swap;
+                }
+            }
+        }
+
+        //for (int i = 0; i < mas.Length; i++)
+        //   Console.WriteLine(mas[i]);
+     
+    }
+
+    public bool Polindrom(string input)
+    {
+        char[] newArr = input.ToCharArray();
+        Array.Reverse(newArr);
+        string s = new string(newArr);
+        if (input == s)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public double Fact(int inputNum)
+    {
+        if (inputNum >= 0)
+        {
+            if(inputNum == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                int count = inputNum;
+                int output = inputNum;
+                while (count > 1)
+                {
+                    count--;
+                    output = output * count;
+                }
+                return output;
+            }
+            
+        }
+        else
+        {
+            return double.NaN;
+        }
+
+    }
+
+    public double Fib(int index)
+    {
+        if (index > 1)
+        {
+            return mas[index - 1] + mas[index - 2];
+        }
+        else
+        {
+            return double.NaN;
+        }
+    }
+
+    public double Vesh(double a, double b)
+    {
+        double c = Math.Round(Math.Pow(a, b), 2);
+        return c;
+    }
+
+    public bool Check(int number)
+    {
+        if (number <= 1) return false; // 1 и меньше не являются простыми числами
+        if (number == 2) return true; // 2 - единственное четное простое число
+
+        if (number % 2 == 0) return false; // все четные числа кроме 2 не являются простыми
+
+        int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+        for (int i = 3; i <= boundary; i += 2)
+        {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+
+    }
+}
+
---
+++ ConsoleApp1Tests/ClassWithFuncTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture()]
+    public class ClassWithFuncTests
+    {
+        ClassWithFunc func = new ClassWithFunc();
+       
+        [TestCase(new int[] {3, 5, 2, 7, 1}, new int[] {1, 2, 3, 5, 7})]
+        [TestCase(new int[] { 1, 8, 3, 4 }, new int[] { 1, 3, 4, 8 })]
+        [TestCase(new int[] {1, 2, 3, 4, 5  }, new int[] { 1, 2,  3, 4, 5 })]
+        public void SortTest(int[] inputMas, int[] extMas)
+        {
+            ClassWithFunc.Sort(ref inputMas);
+            Assert.AreEqual(extMas, inputMas);
+        }
+
+        [TestCase("шалаш", true)]
+        [TestCase("шаллаш", true)]
+        [TestCase("кошка", false)]
+        public void PolindromTest(string input, bool ext)
+        {
+
+            Assert.AreEqual(ext, func.Polindrom(input));
+        }
+
+        [TestCase(5, 120)]
+        [TestCase(-1, double.NaN)]
+        [TestCase(0, 1)]
+        public void FactTest(int inputNum, double ext)
+        {
+            Assert.AreEqual(ext, func.Fact(inputNum));
+        }
+
+        [TestCase(1, double.NaN)]
+        [TestCase(2, 8)]
+        [TestCase(4, 9)]
+        public void FibTest(int index, double ext)
+        {
+            Assert.AreEqual(ext, func.Fib(index));
+        }// 3, 5, 2, 7, 1
+
+        [TestCase(1.5, 4.6, 6.46)]
+        [TestCase(8.5, 1.6, 30.70)]
+        [TestCase(0.05, 4.6, 0)]
+        public void VeshTest(double a, double b, double output)
+        {
+            Assert.AreEqual(output, func.Vesh(a, b)); 
+        }
+
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(13, true)]
+        [TestCase(4, false)]
+        public void CheckTest(int value, bool ext)
+        {
+            Assert.AreEqual(ext, func.Check(value));
+        }
+    }
+}
